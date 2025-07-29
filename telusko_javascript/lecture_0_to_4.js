36c12f046f6883e520b7ef5c6f8a187769529dce
Sandhiya C V <112756649+SandhiyaGiri@users.noreply.github.com>
2025-07-29 22:17:03 +0530

    Create lecture_0_to_4.js

---
+++ telusko_javascript/lecture_0_to_4.js
@@ -0,0 +1,18 @@
+// Lecture 0 - Introduction to Java Script - In older days the websites are static; Java Script is introduced as Frontend
+// But gradually it become go-to language for both frontend and backend - Making the website interactive
+
+// Lecture 1 - Hello world
+console.log("Hello, World");
+
+// Lecture 2 - Installation guide and JS Engine
+
+// Lecture 3 - Variables and Naming rules(Same as python and C/C++)
+let num_1 = 4
+let num_2 = 5
+console.log("num_1 * num_2 = ", num_1*num_2);
+
+// Lecture 4 - Constants
+const pi = 3.14
+let rad = 3
+let circum = 2*pi*rad
+console.log("Circumference = ", circum);
