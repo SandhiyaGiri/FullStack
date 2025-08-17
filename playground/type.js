0d9e0801fc4e751ab555da0b95b37711ef71d0af
SandhiyaGiri <sandhiyagiri03@gmail.com>
2025-08-17 23:09:35 +0530

    Push the code to the repo

---
+++ playground/type.js
@@ -0,0 +1,9 @@
+const a = 12;
+const b = 13;
+
+const san = "sandhiya";
+const dic = {"name": 'Sandhiya C V', "age": 22, "school": "Bharathi"};
+const details = {'first_name': "Sandhiya", 'last_name': "Giri", "middle_name" : null};
+const somethingUndefined;
+somethingUndefined = 8;
+console.log(san, dic)
